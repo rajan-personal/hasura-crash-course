bc8b2c5d4841823254ef0a2d5602da45bcc020db
rajan-personal <39906424+rajan-personal@users.noreply.github.com>
2024-12-21 11:23:02 +0530

    deploy

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-const server = app.listen(8000, () => {
-    console.log("server listening on port 8000");
-});
